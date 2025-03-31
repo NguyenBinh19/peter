15275f9196a972f294777d1e6b486327e564277c
Nguyễn Bình <binhco2004@gmail.com>
2025-03-31 23:05:23 +0700

    init config

--- be/src/main/java/com/example/peter/PeterApplication.java
+++ be/src/main/java/com/example/peter/PeterApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.peter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PeterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PeterApplication.class, args);
 	}
 
 }
